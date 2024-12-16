94b9f39f827f24d149c7ab2be2999ef3a5dce555
Alexey <56958306+dante9990@users.noreply.github.com>
2024-12-16 19:45:30 +0400

    fix recipe

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const RecipeCard: FC<Props> = ({ data }) => {
   return (
-    <Link to={`/recipe/${data.recipeId}`} className={styles.wrapper}>
+    <Link to={`/urban_homework_18/recipe/${data.recipeId}`} className={styles.wrapper}>
       <img src={data.imageUrl} alt="" />
       <div className={styles.info}>
         <h3>{data.name}</h3>
